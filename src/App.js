75a0e8151181b9ee21212652895be7895b0d2bcd
Michael Martin <michaelmartin@Michaels-MacBook-Pro.local>
2022-09-25 23:36:13 -0700

    Update Resume App

--- src/App.js
+++ src/App.js
@@ -36,22 +36,21 @@ function App() {
             
             <Text>
               Mike Martin
-            </Text>
-
-            
-
-            
-          
+            </Text>         
+       
             <ButtonGroup variant='outline' spacing='6'>
               <Link href="https://www.linkedin.com/in/michael-martin-3231b512a/" target="_blank"><Button colorScheme='twitter' leftIcon={<FaLinkedin />}>LinkedIn</Button></Link>
               <Link href="https://github.com/michaelmartin9937" target="_blank"><Button colorScheme='twitter' leftIcon={<FaGithub />}>GitHub</Button></Link>
               <Link href="https://docs.google.com/document/d/1i7QTKKO_dwiCb2wy4U6LQvkwjF5kajaiatSnCAnnyl0/edit?usp=sharing" target="_blank"><Button colorScheme='blue'>Resume</Button></Link>
-            </ButtonGroup>
+            </ButtonGroup>        
 
             <hr></hr>
 
-            <Text>My Best Work</Text>
+            <Text>My Projects</Text>
             <Link href="https://broken-hall-1854.on.fleek.co/" target="_blank" color="#D25A59">Cryptocurrency Exchange</Link>
+            <Link href="https://sparkling-dawn-1591.on.fleek.co/" target="_blank" color="#D25A59">ENS Marketplace</Link>
+            <Link href="https://orange-smoke-9788.on.fleek.co/" target="_blank" color="#D25A59">NFT Project</Link>
+            <Link href="https://github.com/michaelmartin9937/Chainlink-Node" target="_blank" color="#D25A59">Chainlink Node</Link>
 
             
             
